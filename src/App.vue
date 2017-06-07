202794e33ccde1e52f2bd538a37b709ecc6e76af
Olmo Kramer <olmo.kramer@gmail.com>
2017-06-07 21:58:22 +0200

    code-viewer styling

--- src/App.vue
+++ src/App.vue
@@ -17,5 +17,10 @@
         -moz-osx-font-smoothing: grayscale;
         color: #2c3e50;
         margin-top: 60px;
-    }
+        box-sizing: border-box;
+}
+
+* {
+        box-sizing: inherit;
+}
 </style>
--- src/components/CodeViewer.vue
+++ src/components/CodeViewer.vue
@@ -1,12 +1,15 @@
 <template>
-    <ol class="code-viewer">
-        <li v-for="(line, i) in highlighted_code">
-            <code v-html="line"></code>
-            <div class="feedback" v-if="feedback[i]">
-                {{ feedback[i] }}
-            </div>
-        </li>
-    </ol>
+    <div class="code-viewer">
+        <ol>
+            <li v-for="(line, i) in highlighted_code">
+                <code v-html="line"></code>
+                <div class="feedback" v-if="feedback[i]">
+                    {{ feedback[i] }}
+                </div>
+                <span class="add-comment" v-if="!feedback[i]">+</span>
+            </li>
+        </ol>
+    </div>
 </template>
 
 <script>
@@ -62,6 +65,21 @@ export default {
 <style>
 ol {
     font-family: monospace;
+    margin: 0;
+    margin-left: 2em;
+    padding: 0;
+    background: #f8f8f8;
+}
+
+li {
+    position: relative;
+    margin-left: 4em;
+    padding-left: 1em;
+    background: white;
+
+    /* Prevent margin collapse. */
+    /* TODO: Find better solution. */
+    padding-bottom: 1px;
 }
 
 code {
@@ -70,6 +88,51 @@ code {
 }
 
 .feedback {
+    margin: .5em;
+    padding: 1em;
+    border: 1px solid #eee;
+    border-radius: 8px;
     font-family: sans-serif;
+}
+
+/* The '+' button on the left side. */
+.add-comment {
+    display: none;
+    position: absolute;
+    top: 0;
+    right: 100%;
+    width: 1.5em;
+    height: 100%;
+    margin-right: 4em;
+    padding: .2em .5em;
+    background: #f8f8f8;
+    border: 2px solid #f8f8f8;
+    border-right-width: 0;
+    border-radius: 35% 0 0 35%;
+    font-weight: bold;
+    text-align: center;
+    cursor: pointer;
+}
+
+.add-comment:hover {
+        display: block;
+        background: transparent;
+}
+
+li:hover .add-comment {
+    display: block;
+}
+
+/* Need this to be able to move the cursor all
+ * the way to the left to the '+' button without
+ * losing the hover on the li. */
+li::before {
+    display: block;
+    position: absolute;
+    top: 0;
+    right: 100%;
+    width: 4em;
+    height: 100%;
+    content: '';
 }
 </style>
