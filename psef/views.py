fa73f1578fa26107522fdc4285b0a69a4816b147
Olmo Kramer <olmo.kramer@gmail.com>
2017-06-07 20:36:26 +0200

    add code-viewer component

--- psef/views.py
+++ psef/views.py
@@ -6,3 +6,25 @@ from flask import jsonify
 @app.route("/api/hello")
 def say_hello():
     return jsonify({"msg": "Hello this is Flask."})
+
+
+@app.route("/api/code/<id>")
+def get_code(id):
+    print(id)
+    if id == "0":
+        return jsonify({
+            "id": 0,
+            "code": "def id0func0():\n\treturn 0\n\n\ndef id0func1():\n\treturn 1",
+            "feedback": {
+                "0": "wtf",
+            }
+        })
+    else:
+        return jsonify({
+            "id": id,
+            "code": "def some_func():\n\treturn 0\n\n\ndef other_func():\n\treturn 1",
+            "feedback": {
+                "0": "slechte naam voor functie",
+                "4": "niet veel beter..."
+            }
+        })
