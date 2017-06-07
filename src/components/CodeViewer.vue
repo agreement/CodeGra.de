3634a7d1bcd42676bacdf2639363b78211ee2eff
Olmo Kramer <olmo.kramer@gmail.com>
2017-06-07 20:36:26 +0200

    add code-viewer component

---
+++ src/components/CodeViewer.vue
@@ -0,0 +1,70 @@
+<template>
+    <ol class="code-viewer">
+        <li v-for="(line, i) in highlighted_code">
+            <code v-html="line"></code>
+            <div class="feedback" v-if="feedback[i]">
+                {{ feedback[i] }}
+            </div>
+        </li>
+    </ol>
+</template>
+
+<script>
+import { highlight } from 'highlightjs';
+
+export default {
+    name: 'code-viewer',
+
+    data() {
+        return {
+            id: this.$route.params.id,
+            input_code: '',
+            feedback: [],
+        };
+    },
+
+    computed: {
+        highlighted_code() {
+            const highlighted = highlight('python', this.input_code);
+            return highlighted.value.split('\n');
+        },
+    },
+
+    mounted() {
+        this.getCode();
+    },
+
+    watch: {
+        $route(to) {
+            this.id = to.params.id;
+            this.getCode();
+        },
+    },
+
+    methods: {
+        getCode() {
+            this.$http.get(`/api/code/${this.id}`).then((data) => {
+                this.input_code = data.body.code;
+                this.feedback = data.body.feedback;
+            });
+        },
+    },
+};
+</script>
+
+<style src="../../node_modules/highlightjs/styles/github.css"></style>
+
+<style>
+ol {
+    font-family: monospace;
+}
+
+code {
+    white-space: pre;
+    line-height: 1.5;
+}
+
+.feedback {
+    font-family: sans-serif;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Hello from '@/components/Hello';
+import CodeViewer from '@/components/CodeViewer';
 
 Vue.use(Router);
 
@@ -10,6 +11,15 @@ export default new Router({
             path: '/',
             name: 'Hello',
             component: Hello,
+        },
+        {
+            path: '/code/:id',
+            name: 'CodeViewer',
+            component: CodeViewer,
+            data: {
+                code: 'test',
+                feedback: [],
+            },
         },
     ],
 });
