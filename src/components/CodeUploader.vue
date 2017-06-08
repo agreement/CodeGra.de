5d2810ffc6fdf67f3adbaf249b65d9fc02651c51
Sebastian Melzer <studielink@skydivizer.com>
2017-06-08 22:52:14 +0200

    Add styling the code upload system vue
    The dropzone now builds up a list of files instead of blocks.
    If the server responds with an error message this is now shown to the user.
    
    Individual files can not be removed from the upload queue at the moment.

--- src/components/CodeUploader.vue
+++ src/components/CodeUploader.vue
@@ -1,13 +1,21 @@
 <template>
     <div id='upload'>
+        <h2>Submit files: (max: {{options.maxFiles}})</h2>
         <dropzone
           ref='dz'
           id='dz-upload'
           :url=options.url
           :useCustomDropzoneOptions=true
-          :dropzoneOptions=options>
+          :dropzoneOptions=options
+          v-on:vdropzone-error="error">
         </dropzone>
-        <button type="button" v-on:click="submit">Submit</button>
+        <ul id="uploads" class="list-group">
+
+        </ul>
+        <div style='align:center'>
+        <button class="btn btn-primary upload" v-on:click="submit">Upload</button>
+        <button class="btn btn-danger upload" v-on:click="removeAll">Remove all</button>
+        </div>
     </div>
 </template>
 
@@ -15,18 +23,15 @@
     import Dropzone from 'vue2-dropzone';
 
     const htmlTemplate = `
-    <div class="dz-preview dz-file-preview">
-        <div class="dz-image" style="width: 200px;height: 200px"><img data-dz-thumbnail /></div>
-        <div class="dz-details">
-            <div class="dz-filename"><span data-dz-name></span></div>
-            <div class="dz-size"><span data-dz-size></span></div>
-        </div>
-        <div class="dz-progress"><span class="dz-upload" data-dz-uploadprogress></span></div>
-        <div class="dz-error-message"><span data-dz-errormessage></span></div>
-        <div class="dz-success-mark"><i class="fa fa-check"></i></div>
-        <div class="dz-error-mark"><i class="fa fa-close"></i></div>
-    </div>
-`;
+    <li class="list-group-item s-file">
+        <span class="error label label-as-badge label-danger">Upload error</span>
+        <span class="warning label label-as-badge label-warning" data-dz-errormessage></span>
+        <span class="success label label-as-badge label-success">Uploaded</span>
+        <span class="name" data-dz-name></span>
+        <span class="size" data-dz-size></span>
+    </li>
+    `;
+
 
     export default {
         name: 'Upload',
@@ -34,27 +39,23 @@
             Dropzone,
         },
         props: {
-            assignmentId: {
-                type: Number,
-                default: 0,
-            },
             workId: {
                 type: Number,
                 default: 0,
             },
             maxFilesize: {
                 type: Number,
-                default: 16,
+                default: 64,
             },
             maxFiles: {
                 type: Number,
-                default: 1,
+                default: 10,
             },
         },
         data() {
             return {
                 options: {
-                    url: `api/v1/assignments/${this.assignmentId}/works/${this.workId}/file`,
+                    url: `api/v1/works/${this.workId}/file`,
                     method: 'POST',
                     maxFiles: this.maxFiles,
                     maxFilesize: this.maxFilesize,
@@ -62,9 +63,10 @@
                     paramName: 'file',
                     createImageThumbnails: false,
                     autoProcessQueue: false,
-                    addRemoveLinks: true,
-                    dictDefaultMessage: 'Drop code here or click to upload',
+                    addRemoveLinks: false,
+                    dictDefaultMessage: 'Drop code here or click to submit',
                     previewTemplate: htmlTemplate,
+                    // previewsContainer: '#uploads',
                 },
             };
         },
@@ -72,16 +74,87 @@
             submit: function submit() {
                 this.$refs.dz.processQueue();
             },
+            removeAll: function removeAll() {
+                this.$refs.dz.removeAllFiles();
+            },
+            error: function error(file, response) {
+                let message = null;
+                if (typeof response === 'string') {
+                    message = response;
+                } else {
+                    message = response.message;
+                }
+                const child = file.previewElement.children[0];
+                child.innerText = message;
+            },
         },
     };
 </script>
 
 <style>
-.dz-remove {
-    right: 15px;
+.dz-details {
+    width: 100%;
+    background-color: lightblue;
+    padding: 5px;
 }
-.dz-image {
-    width: 200px;
-    height: 200px;
+
+.dz-error .dz-details {
+    background-color: yellow;
 }
+
+.dz-success .dz-details {
+    background-color: lightgreen;
+}
+/*
+.s-file {
+    padding: 5px;
+    margin: 5px;
+    background-color: lightgray;
+}
+*/
+.s-file {
+    overflow: hidden;
+    text-overflow: ellipsis;
+    padding-right: 60px;
+}
+
+.s-file .name {
+    white-space: pre;
+}
+
+.s-file .size {
+    position: absolute;
+    right: 10px;
+    top: 10px;
+}
+
+.s-file .label {
+    text-overflow: ellipsis;
+    overflow: hidden;
+}
+
+.s-file.dz-error.dz-processing .warning, .s-file .warning, .s-file .success, .s-file .error {
+    display: none;
+}
+
+.s-file.dz-error .warning, .s-file.dz-success .success, .s-file.dz-error.dz-processing .error {
+    display: block;
+}
+
+.s-file.dz-success {
+    background-color: #dff0d8;
+    color: #3c763d
+}
+
+.s-file.dz-error {
+    background-color: #fcf8e3;
+    color: #8a6d3b;
+}
+
+.s-file.dz-processing.dz-error {
+    background-color: #f2dede;
+    color: #a94442;
+}
+
+
 </style>
