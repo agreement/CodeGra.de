fae7b3a8a828bb29fdc34ee13da884330543a59b
Leon Bartelsman <Leon@wcw-stud-145-109-19-217.wireless.uva.nl>
2017-06-08 16:33:32 +0200

    grading component halfway

--- psef/views.py
+++ psef/views.py
@@ -6,3 +6,20 @@ from flask import jsonify
 @app.route("/api/hello")
 def say_hello():
     return jsonify({"msg": "Hello this is Flask."})
+
+@app.route("/api/v1/general-feedback/<id>")
+def get_general_feedback(id):
+	print(id)
+	if id == "0":
+		return jsonify({
+			"id": 0,
+			"grade": 8.5,
+			"general_feedback": "test feedback voor id nul"
+		})
+	else:
+		return jsonify({
+			"id": 0,
+			"grade": 6.5,
+			"general_feedback": "test feedback"
+		})
+
--- src/components/GradingAssignment.vue
+++ src/components/GradingAssignment.vue
@@ -1,46 +1,61 @@
 <template>
     <div class="GradingAssignment">
-        <h1>{{ msg }}</h1>
-        <h2>Essential Links</h2>
-        <ul>
-            <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-            <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-            <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-            <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-            <br>
-            <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-        </ul>
-        <h2>Ecosystem</h2>
-        <ul>
-            <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-            <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-            <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-            <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-        </ul>
+        <div class="row">
+            <div class="col-lg-6">
+                <div class="input-group">
+                    <div class="input-group-btn">
+                        <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-on:click="submitGradeAndGenFeedback($event)">Submit All</button>
+                    </div>
+                    <input type="number" step="any" min="0" max="10"  class="form-control" aria-label="Grade" placeholder="Grade">
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="input-group">
+                    <textarea type="text" class="form-control" aria-label="General feedback" placeholder="General feedback" rows="3"></textarea>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
- export default {
-     data() {
-         return {
-             msg: 'Hello this is Vue',
-         };
-     },
-     mounted() {
-         this.$nextTick(() => { this.getMsg(); });
-     },
-     methods: {
-         getMsg() {
-             this.$http.get('/api/hello').then((data) => {
-                 this.msg = data.body.msg;
-             });
-         },
-     },
- };
+export default {
+    name: 'grade-viewer',
+
+    props: ['editable'],
+
+    data() {
+        return {
+            id: this.$route.params.id,
+            grade: this.$route.params.grade,
+            general_feedback: '',
+        };
+    },
+
+    mounted() {
+        if (!this.grade) {
+            this.getGradeAndGenFeedback();
+        }
+    },
+
+    methods: {
+        getGradeAndGenFeedback() {
+            this.$http.get('/api/v1/general-feedback/${this.id}').then((data) => {
+                Object.assign(this, data.body);
+            });
+        },
+
+        submitGradeAndGenFeedback(event) {
+            console.log(event);
+        },
+    },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+.input-group {
+    width: 100%;
+    margin: 5px;
+}
 </style>
