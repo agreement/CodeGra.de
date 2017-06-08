366e370ec6c77e26ab5d023d80bb6a9f9a722a75
Sebastian Melzer <studielink@skydivizer.com>
2017-06-08 14:27:41 +0200

    Extend the code uploader vue
    Calls the correct api, shows the preview template explicitly to allow
    styling/formatting.

--- src/components/CodeUploader.vue
+++ src/components/CodeUploader.vue
@@ -1,39 +1,87 @@
 <template>
     <div id='upload'>
         <dropzone
+          ref='dz'
           id='dz-upload'
           :url=options.url
           :useCustomDropzoneOptions=true
           :dropzoneOptions=options>
-            <!-- <input type="hidden" name="token" value="xxx"> -->
         </dropzone>
+        <button type="button" v-on:click="submit">Submit</button>
     </div>
 </template>
 
 <script>
     import Dropzone from 'vue2-dropzone';
 
+    const htmlTemplate = `
+    <div class="dz-preview dz-file-preview">
+        <div class="dz-image" style="width: 200px;height: 200px"><img data-dz-thumbnail /></div>
+        <div class="dz-details">
+            <div class="dz-filename"><span data-dz-name></span></div>
+            <div class="dz-size"><span data-dz-size></span></div>
+        </div>
+        <div class="dz-progress"><span class="dz-upload" data-dz-uploadprogress></span></div>
+        <div class="dz-error-message"><span data-dz-errormessage></span></div>
+        <div class="dz-success-mark"><i class="fa fa-check"></i></div>
+        <div class="dz-error-mark"><i class="fa fa-close"></i></div>
+    </div>
+`;
+
     export default {
         name: 'Upload',
         components: {
             Dropzone,
         },
+        props: {
+            assignmentId: {
+                type: Number,
+                default: 0,
+            },
+            workId: {
+                type: Number,
+                default: 0,
+            },
+            maxFilesize: {
+                type: Number,
+                default: 16,
+            },
+            maxFiles: {
+                type: Number,
+                default: 1,
+            },
+        },
         data() {
             return {
                 options: {
-                    url: 'api/upload-code',
+                    url: `api/v1/assignments/${this.assignmentId}/works/${this.workId}/file`,
                     method: 'POST',
-                    maxFiles: 1,
-                    maxFilesize: 16,
-                    filesizeBase: 1024 * 1024,
+                    maxFiles: this.maxFiles,
+                    maxFilesize: this.maxFilesize,
+                    filesizeBase: 1000,
                     paramName: 'file',
                     createImageThumbnails: false,
-                    headers: {
-                        student: 11,
-                        assignment: 12,
-                    },
+                    autoProcessQueue: false,
+                    addRemoveLinks: true,
+                    dictDefaultMessage: 'Drop code here or click to upload',
+                    previewTemplate: htmlTemplate,
                 },
             };
         },
+        methods: {
+            submit: function submit() {
+                this.$refs.dz.processQueue();
+            },
+        },
     };
 </script>
+
+<style>
+.dz-remove {
+    right: 15px;
+}
+.dz-image {
+    width: 200px;
+    height: 200px;
+}
+</style>
