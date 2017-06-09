8f38db67f98a7c3d3a9354484d1be53b782399c3
Olmo Kramer <olmo.kramer@gmail.com>
2017-06-09 16:30:17 +0200

    put some content in side-bar

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,10 +1,47 @@
 <template>
-    <div class="side-bar"></div>
+    <nav class="side-bar">
+        <ul v-if="loggedIn">
+            <li><a v-bind:href="userBaseURL">{{ username }}</a></li>
+            <li><a v-bind:href="userOpenURL">Open assignments</a></li>
+            <li><a v-bind:href="userGradedURL">Graded assignments</a></li>
+            <li><a v-bind:href="userSettingsURL">Settings</a></li>
+            <li><a href="#/logout">Logout</a></li>
+        </ul>
+        <ul v-else>
+            <li><a href="#/login">login</a></li>
+        </ul>
+    </nav>
 </template>
 
 <script>
 export default {
     name: 'side-bar',
+
+    data() {
+        return {
+            loggedIn: 1,
+            userid: 0,
+            username: 'Henk',
+        };
+    },
+
+    computed: {
+        userBaseURL() {
+            return `#/users/${this.userid}`;
+        },
+
+        userOpenURL() {
+            return `${this.userBaseURL}/assignments?graded=0`;
+        },
+
+        userGradedURL() {
+            return `${this.userBaseURL}/assignments?graded=1`;
+        },
+
+        userSettingsURL() {
+            return `${this.userBaseURL}/settings`;
+        },
+    },
 };
 </script>
 
