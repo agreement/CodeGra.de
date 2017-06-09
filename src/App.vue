34ee063ad02240f5bbb53cc6e2d517e03b0b1b8a
Olmo Kramer <olmo.kramer@gmail.com>
2017-06-09 16:31:20 +0200

    app layout

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <template>
     <div id="app">
         <header-bar></header-bar>
-        <side-bar></side-bar>
-        <router-view></router-view>
+        <main>
+            <side-bar></side-bar>
+            <router-view></router-view>
+        </main>
     </div>
 </template>
 
@@ -19,15 +21,27 @@ export default {
 };
 </script>
 
-<style>
+<style lang="less">
+@sidebar-width: 10em;
+
 #app {
-        font-family: 'Avenir', Helvetica, Arial, sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-        box-sizing: border-box;
+    box-sizing: border-box;
 }
 
 * {
-        box-sizing: inherit;
+    box-sizing: inherit;
+}
+
+main {
+    display: flex;
+}
+
+.side-bar {
+    flex: 0 0 @sidebar-width;
+}
+
+.page {
+    flex-grow: 1;
+    flex-shrink: 1;
 }
 </style>
