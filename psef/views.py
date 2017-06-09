6cccb787e231f519fd982e2c14aab2b42ae9b9b2
Olmo Kramer <olmo.kramer@gmail.com>
2017-06-09 21:28:18 +0200

    integrate grade-viewer in assignment page
    and rename assignment page -> submssion

--- psef/views.py
+++ psef/views.py
@@ -3,43 +3,12 @@ from psef import app
 from flask import jsonify, request, make_response
 
 
-@app.route("/api/v1/work/<int:work_id>/general-feedback/", methods=['GET', 'PUT'])
-def get_general_feedback(work_id):
-    if request.method == 'GET':
-    	if id == 0:
-    		return jsonify({
-    			"work_id": 0,
-    			"grade": 8.5,
-    			"general_feedback": "test feedback voor id nul"
-    		})
-    	else:
-    		return jsonify({
-    			"work_id": 0,
-    			"grade": 6.5,
-    			"general_feedback": "test feedback"
-    		})
-    elif request.method == 'PUT':
-        content = request.get_json()
-
-        # doe iets in de database
-        print(content)
-
-        resp = make_response("grade and feedback submitted", 204)
-        return resp
-@app.route("/api/v1/students/<student_id>/assignments/<assignment_id>")
-def get_student_assignment(student_id, assignment_id):
-    return jsonify({
-        "title": "Assignment 1",
-        "due_date": "1497020367",
-        "fileTree": dir_contents("abc"),
-    })
 
 
 @app.route("/api/v1/code/<id>")
 def get_code(id):
     if id == "0":
         return jsonify({
-            "id": 0,
             "lang": "python",
             "code": "def id0func0():\n\treturn 0\n\n\ndef id0func1():\n\t" +
                     "return 1",
@@ -49,7 +18,6 @@ def get_code(id):
         })
     else:
         return jsonify({
-            "id": id,
             "lang": "c",
             "code": "void\nsome_func(void) {}\n\nvoid other_func(int x)" +
                     "{\n\treturn 2 * x;\n}",
@@ -87,4 +55,34 @@ def dir_contents(path):
             {"name": "d", "id": 5},
         ]
     }
->>>>>>> master
+
+
+@app.route("/api/v1/submission/<submission_id>")
+def get_submission(submission_id):
+    return jsonify({
+        "title": "Assignment 1",
+        "fileTree": dir_contents("abc"),
+    })
+
+
+@app.route("/api/v1/submission/<submission_id>/general-feedback", methods=['GET', 'PUT'])
+def get_general_feedback(submission_id):
+    if request.method == 'GET':
+    	if id == 0:
+    		return jsonify({
+    			"grade": 8.5,
+    			"feedback": "test feedback voor id nul"
+    		})
+    	else:
+    		return jsonify({
+    			"grade": 6.5,
+    			"feedback": "test feedback"
+    		})
+    elif request.method == 'PUT':
+        content = request.get_json()
+
+        # doe iets in de database
+        print(content)
+
+        resp = make_response("grade and feedback submitted", 204)
+        return resp
