ed62e9be2ba730b5f64b2f54ebd04ff6c10fee36
Youri Voet <youri@yourivoet.com>
2017-06-09 21:50:53 +0200

    Fix pep8 for views.py

--- psef/views.py
+++ psef/views.py
@@ -7,21 +7,23 @@ from flask import jsonify, request, make_response
 def say_hello():
     return jsonify({"msg": "Hello this is Flask."})
 
-@app.route("/api/v1/work/<int:work_id>/general-feedback/", methods=['GET', 'PUT'])
+
+@app.route("/api/v1/work/<int:work_id>/general-feedback/",
+           methods=['GET', 'PUT'])
 def get_general_feedback(work_id):
     if request.method == 'GET':
-    	if id == 0:
-    		return jsonify({
-    			"work_id": 0,
-    			"grade": 8.5,
-    			"general_feedback": "test feedback voor id nul"
-    		})
-    	else:
-    		return jsonify({
-    			"work_id": 0,
-    			"grade": 6.5,
-    			"general_feedback": "test feedback"
-    		})
+        if id == 0:
+            return jsonify({
+                "work_id": 0,
+                "grade": 8.5,
+                "general_feedback": "test feedback voor id nul"
+            })
+        else:
+            return jsonify({
+                "work_id": 0,
+                "grade": 6.5,
+                "general_feedback": "test feedback"
+            })
     elif request.method == 'PUT':
         content = request.get_json()
 
