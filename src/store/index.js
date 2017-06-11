69ef0298f790d911c3a9b0ec808ca1e864e8cd63
Youri Voet <youri@yourivoet.com>
2017-06-11 13:42:21 +0200

    Remove commented code

--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,12 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 
-// import createPersistedState from 'vuex-persistedstate';
-
 import user from './modules/user';
 
 Vue.use(Vuex);
 
 const debug = process.env.NODE_ENV !== 'production';
-const plugins = [
-    // createPersistedState(),
-];
+const plugins = [];
 
 export default new Vuex.Store({
     modules: {
