15df1ddb371d5ffd50a790dbd9f90518cac681b6
Leon Bartelsman <Leon@wcw-stud-145-109-41-32.wireless.uva.nl>
2017-06-14 14:54:32 +0200

    Redirect to submission page after uploading

--- src/components/CodeUploader.vue
+++ src/components/CodeUploader.vue
@@ -106,6 +106,7 @@
                     uploadMultiple: true,
                     parallelUploads: this.maxFiles,
                     previewsContainer: '#queue',
+                    successmultiple: this.successmMultiple,
                 },
             };
         },
@@ -173,12 +174,15 @@
                 file.previewElement.classList.remove('list-group-item-info');
                 file.previewElement.classList.add('list-group-item-success');
             },
+            successmMultiple: function successmMultiple(files, response) {
+                console.log(response);
+                this.$router.push({ name: 'submission', params: { submissionId: response.id } });
+            },
             queueComplete: function queueComplete() {
                 this.$refs.progress.classList.remove('progress-bar-striped');
                 this.$refs.progress.classList.remove('progress-bar-animated');
                 this.$refs.progress.innerHTML = 'Done';
             },
-
         },
     };
 </script>
