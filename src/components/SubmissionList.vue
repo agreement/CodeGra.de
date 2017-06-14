8b0ad41da68019e6e3ed57d5caa8621dede26dce
Olmo Kramer <olmo.kramer@gmail.com>
2017-06-14 12:51:53 +0200

    submission-list: minor styling fixes

--- src/components/SubmissionList.vue
+++ src/components/SubmissionList.vue
@@ -110,18 +110,21 @@ export default {
 </script>
 
 <style lang="less">
-.table tr {
-    cursor: pointer;
-}
-.table tbody:empty::after {
-    content: 'No results found!';
-    text-align: center;
-    position: absolute;
-    left: 0;
-    right: 0;
-    padding-top: 1em;
-    padding-bottom: 1em;
-    background: #eceeef;
-    margin: 0 15px;
+.table {
+    position: relative;
+
+    tbody:empty::after {
+        content: 'No results found!';
+        text-align: center;
+        position: absolute;
+        left: 0;
+        right: 0;
+        padding: .75rem;
+        background: #eceeef;
+    }
+
+    tr {
+        cursor: pointer;
+    }
 }
 </style>
