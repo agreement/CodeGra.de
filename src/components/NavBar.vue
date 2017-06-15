09f140a5942054fa777ca5fe1c82a55c477f113d
Leon Bartelsman <Leon@wcw-stud-145-109-1-215.wireless.uva.nl>
2017-06-15 15:35:45 +0200

    align login and add navBar-styling-

---
+++ src/components/NavBar.vue
@@ -0,0 +1,106 @@
+<template>
+
+    <b-navbar toggleable type="inverse" sticky="true" class="navbar">
+
+        <b-nav-toggle target="nav_collapse"></b-nav-toggle>
+
+        <b-link class="navbar-brand" to="#">
+            <router-link :to="{ name: 'home', }">
+                <img class="logo" src="/static/img/codegrade.svg">
+            </router-link>
+        </b-link>
+
+        <b-collapse is-nav id="nav_collapse">
+            
+            <div v-if="loggedIn" class="loggedin-nav">
+                <b-nav is-nav-bar class="navbar-left">
+                    <router-link class="nav-item" tag="li" :to="{ name: 'me', params: { userId: this.userid, }, }" active-class="active">
+                        {{username}}
+                    </router-link>
+                    <router-link class="nav-item" tag="li" :to="{ name: 'assignments', }"  active-class="active">
+                        Assignments
+                    </router-link>
+                </b-nav>
+                <b-nav is-nav-bar class="navbar-right">
+                    <router-link class="nav-item" tag="li" :to="{ name: 'logout', }" @click.native.capture="logout"  active-class="active">
+                        Logout
+                    </router-link>
+                </b-nav>
+            </div>
+            <b-nav is-nav-bar v-else>
+                <router-link class="nav-item" tag="li" :to="{ name: 'login', }"  active-class="active">
+                    Login
+                </router-link>
+            </b-nav>
+
+        </b-collapse>
+    </b-navbar>
+
+</template>
+
+<script scoped>
+import { mapGetters, mapActions } from 'vuex';
+
+export default {
+    name: 'nav-bar',
+
+    computed: {
+        ...mapGetters('user', {
+            loggedIn: 'loggedIn',
+            userid: 'id',
+            username: 'name',
+        }),
+    },
+
+    methods: {
+        ...mapActions('user', [
+            'logout',
+        ]),
+    },
+};
+</script>
+
+<style lang="scss">
+
+.navbar {
+    background-color: #2c3e50;
+    margin-bottom: 2em;
+}
+
+.loggedin-nav {
+    width: 100%;
+}
+
+@media (min-width: 768px) {
+    .navbar-left {
+        float: left;
+    }
+
+    .navbar-right {
+        float: right;
+    }
+}
+
+.navbar-collapse li {
+    text-align: right;
+    color: #FFF;
+}
+
+.active {
+    border-bottom: 3px solid white;
+}
+
+.nav-item {
+    padding: 0.5em;
+}
+
+.nav-item:hover {
+    color: #cbcbcb;
+}
+
+.logo {
+    width: 10em;
+}
+
+</style>
+
