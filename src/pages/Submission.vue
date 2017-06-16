0f30e4be27da4f9fde62489e2247679f81f402cc
Youri Voet <youri@yourivoet.com>
2017-06-16 16:46:55 +0200

    Fix some layout issues

--- src/pages/Submission.vue
+++ src/pages/Submission.vue
@@ -1,9 +1,8 @@
 <template>
     <div class="page submission">
-        <h1>{{ title }}</h1>
-
-        <div class="row code-browser">
-            <div class="col-10 code-and-grade">
+        <div class="row justify-content-center code-browser">
+            <h1>{{ title }}</h1>
+            <div class="col-8 code-and-grade">
                 <code-viewer class="" v-bind:editable="editable"
                     v-bind:id="fileId" v-if="fileId" ref="codeViewer"></code-viewer>
                 <grade-viewer v-bind:id="submissionId" :editable="editable"
