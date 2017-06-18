c9f82379e9cd35e8aae22ba9f41d665c58296d85
Wicher Heldring <wicher.heldring@gmail.com>
2017-06-18 21:31:26 +0200

    Add axios error handling on connection errors

--- src/main.js
+++ src/main.js
@@ -10,14 +10,38 @@ import Vue from 'vue';
 import { mapActions } from 'vuex';
 import BootstrapVue from 'bootstrap-vue';
 import axios from 'axios';
+import Toasted from 'vue-toasted';
 
 import App from '@/App';
 import router from '@/router';
 import store from './store';
 
 Vue.use(BootstrapVue);
+Vue.use(Toasted);
 
 Vue.config.productionTip = false;
+
+axios.interceptors.response.use(response => response, (error) => {
+    if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        return Promise.reject(error);
+    }
+
+    if (error.request) {
+        // The request was made but no response was received
+        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+        // http.ClientRequest in node.js
+        Vue.toasted.error('There was an error connecting to the server... Please try again later', { position: 'bottom-center', duration: 1500 });
+        return Promise.reject(error);
+    }
+    // Something happened in setting up the request that triggered an Error
+
+    // eslint-disable-next-line
+    console.error('Error while setting up request', error);
+    return Promise.reject(error);
+});
+
 
 Vue.prototype.$http = axios;
 
