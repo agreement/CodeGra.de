30865fa5613869bc831473055d13f47527c703c2
Olmo Kramer <olmo.kramer@gmail.com>
2017-06-26 00:42:50 +0200

    add filter on the course page

--- src/components/ManageCourse.vue
+++ src/components/ManageCourse.vue
@@ -1,6 +1,14 @@
 <template>
     <b-list-group class="manage-course">
-        <b-list-group-item v-for="a in assignments" :key="a.id">
+        <b-form-fieldset>
+            <b-input-group>
+                <b-form-input v-model="filter"
+                    placeholder="Type to search"
+                    @keyup.enter="submit"></b-form-input>
+            </b-input-group>
+        </b-form-fieldset>
+        <b-list-group-item v-for="a in assignments" :key="a.id"
+            v-if="shouldShowAssignment(a)">
             <manage-assignment :assignment="a"></manage-assignment>
         </b-list-group-item>
     </b-list-group>
@@ -18,6 +26,26 @@ export default {
         assignments: {
             type: Array,
             default: [],
+        },
+    },
+
+    data() {
+        return {
+            filter: this.$route.query.q,
+        };
+    },
+
+    methods: {
+        shouldShowAssignment(assignment) {
+            if (!this.filter) return true;
+            const name = assignment.name.toLowerCase();
+            return name.indexOf(this.filter.toLowerCase()) > -1;
+        },
+
+        submit() {
+            const query = {};
+            if (this.filter) query.q = this.filter;
+            this.$router.replace({ query });
         },
     },
 
