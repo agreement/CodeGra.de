9bb75c92233965d5c758f02a1ea0abf03afc885d
Olmo Kramer <olmo.kramer@gmail.com>
2017-06-26 15:31:15 +0200

    Fix minor styling issues on home page

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,44 +1,36 @@
 <template>
     <div class="page home">
-        <b-jumbotron fluid="True" header="Code grading made intuitive" lead="The best code grader & feedback tool on the web" >
-            <b-btn variant="primary" size="lg" class="btn-outline-secondary" href="#">Sign up</b-btn>
+        <b-jumbotron :fluid="true" header="Code grading made intuitive" lead="The best code grader & feedback tool on the web" >
+            <b-btn variant="primary" size="lg">Sign up</b-btn>
         </b-jumbotron>
         <div class="container">
             <h3>Made with ❤️ & 🍺</h3>
-            <p>Getting feedback as computer science students was frustating and non-intuitive. All online feedback systems where designed for the less technically focused majors. We decided to fill the gap with our revolutionary new grading tool. This project started as a school assignment for the course "Project Software Engineering" (year 2016-2017). With our tool one can grade code line-by-line. The tool is compatible with the LTI protocol, which makes it possible to easily integrate it in learning environments such as Canvas or Blackboard.</p>
-            <br>
+            <p>
+                Getting feedback as computer science students was frustating and non-intuitive. All online feedback systems where designed for the less technically focused majors. We decided to fill the gap with our revolutionary new grading tool. This project started as a school assignment for the course "Project Software Engineering" (year 2016-2017). With our tool one can grade code line-by-line. The tool is compatible with the LTI protocol, which makes it possible to easily integrate it in learning environments such as Canvas or Blackboard.
+            </p>
             <div class="row text-center">
                 <div class="col-sm-4">
-                    <p class="text-center"><strong>Line per line feedback</strong></p><br>
-                    <a href="#demo" data-toggle="collapse">
-                    <img src="/static/img/screenshot.png" class="rounded-circle" alt="Random Name" width="255" height="255">
-                    </a>
-                    <div id="demo" class="collapse">
-                        <p>Guitarist and Lead Vocalist</p>
-                        <p>Loves long walks on the beach</p>
-                        <p>Member since 1988</p>
+                    <p>
+                        <strong>Line per line feedback</strong>
+                    </p>
+                    <div class="rounded-circle feature-img">
+                        <img src="/static/img/feature-1.png">
                     </div>
                 </div>
                 <div class="col-sm-4">
-                    <p class="text-center"><strong>File browser</strong></p><br>
-                    <a href="#demo2" data-toggle="collapse">
-                    <img src="/static/img/screenshot.png" class="rounded-circle" alt="Random Name" width="255" height="255">
-                    </a>
-                    <div id="demo2" class="collapse">
-                        <p>Drummer</p>
-                        <p>Loves drummin'</p>
-                        <p>Member since 1988</p>
+                    <p>
+                        <strong>File browser</strong>
+                    </p>
+                    <div class="rounded-circle feature-img">
+                        <img src="/static/img/feature-2.png">
                     </div>
                 </div>
                 <div class="col-sm-4">
-                    <p class="text-center"><strong>Easy course management</strong></p><br>
-                    <a href="#demo3" data-toggle="collapse">
-                    <img src="/static/img/screenshot.png" class="rounded-circle" alt="Random Name" width="255" height="255">
-                    </a>
-                    <div id="demo3" class="collapse">
-                        <p>Bass player</p>
-                        <p>Loves math</p>
-                        <p>Member since 2005</p>
+                    <p>
+                        <strong>Easy course management</strong>
+                    </p>
+                    <div class="rounded-circle feature-img">
+                        <img src="/static/img/feature-3.png">
                     </div>
                 </div>
             </div>
@@ -68,28 +60,35 @@ export default {
 };
 </script>
 
-<style>
+<style lang="less" scoped>
 .jumbotron {
-    /* top, transparent, gradient */
     background: linear-gradient(
-        rgba(0, 0, 0, 0.6),
-        rgba(0, 0, 0, 0.6)
-    ),
-    /* bottom, image */
-    url('/static/img/screenshot4.png') no-repeat center center;
+            rgba(0, 0, 0, 0.6),
+            rgba(0, 0, 0, 0.6)
+        ),
+        url('/static/img/screenshot.png') no-repeat center center;
     color: white;
+
+    h1 {
+        font-size: 50px;
+        text-transform: uppercase;
+        font-weight: 700;
+        font-family: Montserrat,"Helvetica Neue",Helvetica,Arial,sans-serif;
+    }
 }
 
-.jumbotron h1 {
-    font-size: 50px;
-    text-transform: uppercase;
-    font-weight: 700;
-    font-family: Montserrat,"Helvetica Neue",Helvetica,Arial,sans-serif;
-}
+.feature-img {
+    display: inline-block;
+    @size: 255px;
+    width: @size;
+    height: @size;
+    overflow: hidden;
+    border: 1px solid #2c3e50;
 
-.rounded-circle {
-    border-width: 1px;
-    border-style: solid;
-    border-color: @color-primary;
+    img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+    }
 }
 </style>
