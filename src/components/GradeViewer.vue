02657f3ea9cfb496d1bd09332fab97b8d122ec91
Olmo Kramer <olmo.kramer@gmail.com>
2017-06-28 23:20:57 +0200

    show grade and points when assignment has a rubric

--- src/components/GradeViewer.vue
+++ src/components/GradeViewer.vue
@@ -171,8 +171,9 @@ export default {
         rubricSelected() {
             const total = this.$refs.rubricViewer.totalPoints();
             const max = this.$refs.rubricViewer.maxPoints();
-            this.grade = (10 * (total / max)).toFixed(2);
-            this.rubricPoints = `${total} / ${max}`;
+            const grade = (10 * (total / max)).toFixed(2);
+            this.grade = grade;
+            this.rubricPoints = `${grade} (${total} / ${max})`;
         },
     },
 
