769ae6b5d492c9692d4235fa340dae9db3541fd0
Olmo Kramer <olmo.kramer@gmail.com>
2017-09-09 00:08:43 +0200

    Add "delete course" button on course list page

--- src/components/CourseList.vue
+++ src/components/CourseList.vue
@@ -1,8 +1,9 @@
 <template>
-    <div>
+    <loader class="text-center" v-if="courses == null"/>
+    <div class="course-list" v-else>
         <div class="row">
             <b-input-group class="col-12">
-                <b-form-input v-model="filter" placeholder="Type to Search" v-on:keyup.enter="submit"></b-form-input>
+                <b-form-input v-model="filter" placeholder="Type to Search" v-on:keyup.enter="submit"/>
             </b-input-group>
         </div>
 
@@ -17,22 +18,74 @@
             <template slot="name" scope="item">
                 {{item.value ? item.value : '-'}}
             </template>
+
             <template slot="role" scope="item">
                 {{item.value ? item.value : '-'}}
             </template>
+
             <template slot="actions" scope="item">
                 <b-btn-group>
-                    <b-popover class="btn-popover" placement="top" content="View assignments" triggers="hover">
-                        <b-btn size="sm" variant="success" @click.stop="gotoCourse(item.item)">
-                            <icon name="list"></icon>
+                    <b-popover class="btn-popover"
+                               placement="top"
+                               content="View assignments"
+                               triggers="hover">
+                        <b-btn class="action-button"
+                               size="sm"
+                               variant="success"
+                               @click.stop="gotoCourse(item.item)">
+                            <icon name="list"/>
                         </b-btn>
                     </b-popover>
-                    <b-popover class="btn-popover" placement="top" content="Manage course" triggers="hover">
-                        <b-btn v-if="item.item.manageable" @click.stop="gotoCourseEdit(item.item) "size="sm" variant="warning">
-                            <icon name="pencil"></icon>
+                    <b-popover class="btn-popover"
+                               placement="top"
+                               content="Manage course"
+                               triggers="hover"
+                               v-if="canManage[item.item.id]">
+                        <b-btn class="action-button"
+                               @click.stop="gotoCourseEdit(item.item)"
+                               size="sm"
+                               variant="warning">
+                            <icon name="pencil"/>
                         </b-btn>
+                    </b-popover>
+                    <b-popover class="btn-popover"
+                            placement="top"
+                            content="Delete course"
+                            triggers="hover"
+                            v-if="canDelete">
+                        <b-button class="action-button"
+                                size="sm"
+                                variant="danger"
+                                @click.stop="$root.$emit('show::modal',`modal_delete-${item.item.id}`)"
+                                label="">
+                            <icon name="times"/>
+                        </b-button>
                     </b-popover>
                 </b-btn-group>
+                <div v-if="canDelete">
+                    <b-modal :id="`modal_delete-${item.item.id}`"
+                             title="Are you sure?"
+                             :hide-footer="true">
+                        <p style="text-align: center;">
+                            By deleting this course, all related information
+                            will be lost forever! This includes assignments,
+                            submissions etc. Are you reallysure you want to
+                            delete this course?
+                        </p>
+                        <b-button-toolbar justify>
+                            <submit-button class="delete-confirm"
+                                           :ref="`deleteButton-${item.item.id}`"
+                                           default="outline-danger"
+                                           @click="deleteCourse(item.item)"
+                                           label="Yes"/>
+                            <b-btn class="text-center"
+                                   variant="success"
+                                   @click="$root.$emit('hide::modal', `modal_delete-${item.item.id}`)">
+                                No!
+                            </b-btn>
+                        </b-button-toolbar>
+                    </b-modal>
+                </div>
             </template>
             <template slot="empty">
                 No results found.
@@ -42,22 +95,23 @@
 </template>
 
 <script>
+import { mapActions } from 'vuex';
 import Icon from 'vue-awesome/components/Icon';
 import 'vue-awesome/icons/pencil';
 import 'vue-awesome/icons/list';
+import 'vue-awesome/icons/times';
+
+import Loader from './Loader';
+import SubmitButton from './SubmitButton';
 
 export default {
     name: 'course-list',
 
-    props: {
-        courses: {
-            type: Array,
-            default: [],
-        },
-    },
-
     data() {
         return {
+            courses: null,
+            canDelete: false,
+            canManage: [],
             filter: null,
             currentPage: 1,
             fields: {
@@ -79,6 +133,25 @@ export default {
 
     mounted() {
         this.filter = this.$route.query.q;
+
+        this.$http.get('/api/v1/permissions/', {
+            params: {
+                permission: 'can_manage_course',
+                course_id: 'all',
+            },
+        }).then(({ data: canManage }) => {
+            this.canManage = canManage;
+        });
+
+        this.hasPermission({
+            name: 'can_create_courses',
+        }).then((canDelete) => {
+            this.canDelete = canDelete;
+        });
+
+        this.$http.get('/api/v1/courses/').then(({ data: courses }) => {
+            this.courses = courses;
+        });
     },
 
     methods: {
@@ -100,28 +173,45 @@ export default {
             });
         },
 
+        deleteCourse(course) {
+            this.$refs[`deleteButton-${course.id}`].submit(
+                this.$http.delete(`/api/v1/courses/${course.id}`).catch(({ response }) => {
+                    throw response.data.message;
+                }),
+            ).then(() => {
+                this.$root.$emit('hide::modal', `modal_delete-${course.id}`);
+                this.courses = this.courses.filter(c => c.id !== course.id);
+            });
+        },
+
 
         submit() {
             if (this.filter) {
                 this.$router.replace({ query: { q: this.filter } });
             }
         },
+
+        ...mapActions({
+            hasPermission: 'user/hasPermission',
+        }),
     },
 
     components: {
         Icon,
+        Loader,
+        SubmitButton,
     },
 };
 </script>
 
 <style lang="less">
 .btn-popover {
-    &:not(:last-child) .btn {
+    &:not(:last-child) .action-button {
         border-top-right-radius: 0;
         border-bottom-right-radius: 0;
     }
 
-    &:not(:first-child) .btn {
+    &:not(:first-child) .action-button {
         border-top-left-radius: 0;
         border-bottom-left-radius: 0;
     }
--- src/pages/Courses.vue
+++ src/pages/Courses.vue
@@ -1,48 +1,23 @@
 <template>
     <div class="page courses">
-        <loader class="text-center" v-if="loading"></loader>
-        <div class="" v-else>
-            <h1>Courses</h1>
-            <course-list :courses="courses"></course-list>
-        </div>
+        <h1>Courses</h1>
+        <course-list/>
     </div>
 </template>
 
 <script>
-import { CourseList, Loader } from '@/components';
-
+import { CourseList } from '@/components';
 import { setPageTitle } from './title';
 
 export default {
     name: 'course-list-page',
 
-    data() {
-        return {
-            loading: true,
-            courses: [],
-        };
-    },
-
     mounted() {
         setPageTitle('Courses');
-
-        Promise.all([
-            this.$http.get('/api/v1/courses/'), this.$http.get('/api/v1/permissions/', {
-                params: { permission: 'can_manage_course', course_id: 'all' },
-            })]).then(([coursesResponse, permsResponse]) => {
-                this.courses = coursesResponse.data;
-                this.loading = false;
-                for (let i = 0; i < this.courses.length; i += 1) {
-                    const course = this.courses[i];
-                    course.manageable = permsResponse.data[course.id];
-                }
-            },
-        );
     },
 
     components: {
         CourseList,
-        Loader,
     },
 };
 </script>
