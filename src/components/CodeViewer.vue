faf7435bd2b0e6ad557d9b21ef9e73a18d76a77a
Thomas Schaper <libre-man@users.noreply.github.com>
2017-09-17 13:23:30 +0200

    Fix line wrapping in code viewer (#217)
    * Fix line wrapping in code viewer
    
    This was done by adding the `<wbr>` tag after each sequence of spaces.

--- src/components/CodeViewer.vue
+++ src/components/CodeViewer.vue
@@ -327,10 +327,10 @@ export default {
                     i += 1;
                 } else if (line[i] === ' ') {
                     while (line[i] === ' ' && i < line.length) i += 1;
-                    newLine.push(`<span class="whitespace space">${Array((i - start) + 1).join('&middot;')}</span>`);
+                    newLine.push(`<span class="whitespace space">${Array((i - start) + 1).join('&middot;')}</span><wbr>`);
                 } else if (line[i] === '\t') {
                     while (line[i] === '\t' && i < line.length) i += 1;
-                    newLine.push(`<span class="whitespace tab">${Array((i - start) + 1).join('&#10230;   ')}</span>`);
+                    newLine.push(`<span class="whitespace tab">${Array((i - start) + 1).join('&#10230;   ')}</span><wbr>`);
                 } else {
                     while (line[i] !== '<' && line[i] !== ' ' && line[i] !== '\t' && i < line.length) i += 1;
                     newLine.push(line.slice(start, i));
@@ -509,7 +509,6 @@ li {
 
 code {
     white-space: pre-wrap;
-    word-break: break-word;
 }
 
 .add-feedback {
