0762f217dcc9f7f3d838c25ba2cebc997412c88e
Olmo Kramer <olmo.kramer@gmail.com>
2017-09-18 00:43:51 +0200

    Color-code http status codes in run.py output (#223)

--- .scripts/start_dev.sh
+++ .scripts/start_dev.sh
@@ -9,5 +9,13 @@ elif [[ "$1" = python ]]; then
     if [[ -z "$VIRTUAL_ENV" ]]; then
         source ./env/bin/activate
     fi
-    ./run.py
+
+    if [ "$TERM" != dumb ] && hash tput 2>/dev/null; then
+        ./run.py 2>&1 | sed \
+            -e 's/ \(2[[:digit:]]\{2\}\) -/ '$(tput setaf 2)'\1'$(tput sgr0)' -/' \
+            -e 's/ \(3[[:digit:]]\{2\}\) -/ '$(tput setaf 3)'\1'$(tput sgr0)' -/' \
+            -e 's/ \([45][[:digit:]]\{2\}\) -/ '$(tput setaf 1)'\1'$(tput sgr0)' -/'
+    else
+        ./run.py
+    fi
 fi
